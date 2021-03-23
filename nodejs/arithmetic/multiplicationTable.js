455f834a22fc10fb0907e076d299ae4ce2466f37
sharon <vwsharon@live.com>
2021-03-24 00:59:08 +0530

    Multiplication Table

--- nodejs/arithmetic/multiplicationTable.js
+++ nodejs/arithmetic/multiplicationTable.js
@@ -2,9 +2,9 @@ const prompt = require('prompt-sync')()
 
 let num = Number(prompt('Enter number :: '))
 
-let range = Number(prompt('Enter range :: '))
+let limit = Number(prompt('Enter limit :: '))
 
-for (let i = 1; i <= range; i++) {
+for (let i = 1; i <= limit; i++) {
 
     console.log('%d * %d = %d', num, i, (num * i))
 }
